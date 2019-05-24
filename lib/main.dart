63badae28a94b994388f9be3aa9e7af1e4ef47b0
SirawichDev <sirawit0676@gmail.com>
2019-05-25 00:22:11 +0700

    score finished

--- lib/main.dart
+++ lib/main.dart
@@ -106,9 +106,46 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-  Widget scorer(){
-    return Container(
 
+  Widget scorer() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 32,top: 24.0,right: 16.0),
+      child: Container(
+        width: double.infinity,
+        height: 130.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.white
+        ),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("200",style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+                    Text("rank",style: TextStyle(fontSize: 15.0,color: Colors.pink.withOpacity(.5),fontWeight: FontWeight.w500),)
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("4000",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),),
+                    Text("points",style: TextStyle(fontWeight: FontWeight.w500,color:Colors.pink.withOpacity(.5)),)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
