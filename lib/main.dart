1c8a8fe86c9acfb91354966a8423b2982ee86a94
SirawichDev <sirawit0676@gmail.com>
2019-05-29 22:27:51 +0700

    fixed over pixel bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:io' show Platform;
+
 void main() {
   return runApp(MaterialApp(
     home: HomePage(),
@@ -76,7 +77,7 @@ class HomePage extends StatelessWidget {
 
   Widget userComponent() {
     return Padding(
-      padding: const EdgeInsets.only(left: 32.0, top: 16.0),
+      padding: const EdgeInsets.only(left: 32.0,),
       child: Row(
         children: <Widget>[
           Container(
@@ -116,10 +117,10 @@ class HomePage extends StatelessWidget {
 
   Widget scorerComponent() {
     return Padding(
-      padding: const EdgeInsets.only(left: 32, top: 30.0, right: 16.0),
+      padding: const EdgeInsets.only(left: 32, top: 10.0, right: 16.0),
       child: Container(
         width: double.infinity,
-        height: 125.0,
+        height: 120.0,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20.0), color: Colors.white),
         child: ClipRRect(
@@ -183,149 +184,147 @@ class HomePage extends StatelessWidget {
 
   Widget graphComponent() {
     return Expanded(
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 32.0, top: 30.0, right: 16.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Text(
-                  "Days",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w700),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Text(
-                  "Months",
-                  style: TextStyle(color: Colors.white, fontSize: 20.0),
-                ),
-                Expanded(
-                  child: Text(
-                    "contribute per months",
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(.8), fontSize: 12.0),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: double.infinity,
-              height: 200.0,
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 32.0, top: 10.0, right: 16.0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  chartComponent("13.20", 40),
-                  chartComponent("10.20", 30),
-                  chartComponent("2.20", 80),
-                  chartComponent("15.20", 60),
-                  chartComponent("2.20", 20),
-                  chartComponent("15.20", 50),
-                  chartComponent("19.20", 90),
-                  chartComponent("19.20", 90),
+                  Text(
+                    "Days",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Text(
+                    "Months",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "contribute per months",
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(.8), fontSize: 12.0),
+                    ),
+                  )
                 ],
               ),
             ),
-          )
-        ],
+          SizedBox(height: 10.0,),
+            Expanded(
+              child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                return Container(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      chartComponent("13.20", 67, constraints.maxHeight),
+                      chartComponent("1.20", 100, constraints.maxHeight),
+                      chartComponent("11.20", 20, constraints.maxHeight),
+                      chartComponent("13.20", 50, constraints.maxHeight),
+                      chartComponent("15.20", 34, constraints.maxHeight),
+                      chartComponent("13.20", 70, constraints.maxHeight),
+                    ],
+                  ),
+                );
+              }),
+            )
+          ],
+        ),
       ),
     );
   }
 
-  Widget chartComponent(String day, int value) {
-    var cHeight = 150.0 * value / 100.0;
-    var offsety = 200.0 - cHeight - 60.0;
-    return Expanded(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: offsety,
-          ),
-          Text(
-            value.toString(),
-            style: TextStyle(
-                fontSize: 12.0,
-                color: value > 50 ? Colors.cyanAccent : Colors.yellow),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Container(
-            width: 2.0,
-            height: cHeight,
-            color: value > 50 ? Colors.cyanAccent : Colors.yellowAccent,
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            day,
-            style:
-                TextStyle(fontSize: 12.0, color: Colors.white.withOpacity(.8)),
-          )
-        ],
-      ),
+  Widget chartComponent(String day, int value, double maxHeight) {
+    var cHeight = maxHeight * value / 140.0;
+    var offsety = maxHeight - cHeight - 60.0;
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: offsety,
+        ),
+        Text(
+          value.toString(),
+          style: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.bold,
+              color: value > 50 ? Colors.cyanAccent : Colors.yellow),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+          width: 2.0,
+          height: cHeight,
+          color: value > 50 ? Colors.cyanAccent : Colors.yellowAccent,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          day,
+          style: TextStyle(fontSize: 12.0, color: Colors.white.withOpacity(.8)),
+        )
+      ],
     );
   }
 }
 
 class FriendComponent extends StatelessWidget {
-
   @override
-
   Widget build(BuildContext context) {
-    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334, allowFontScaling: true)..init(context);
     return Padding(
-      padding: const EdgeInsets.only(top: 30.0),
+      padding: const EdgeInsets.only(top: 5.0),
       child: Container(
-        height: Platform.isAndroid ? MediaQuery.of(context).size.height / 5: MediaQuery.of(context).size.height / 4,
+        height:  130.0,
         decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(30.0))),
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0))),
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Padding(
-                padding: Platform.isAndroid ? EdgeInsets.all(20.0): EdgeInsets.all(30.0),
-                child: Text(
-                  "Friends",
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
+              Text(
+                "Friends",
+                style: TextStyle(fontWeight: FontWeight.w700),
               ),
+              SizedBox(height: 10.0,),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
                   Container(
-                    width: 75.0,
-                    height: 75.0,
+                    width: 60.0,
+                    height: 60.0,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(25.0),
                         image: DecorationImage(
                             image: AssetImage('assets/x.jpeg'))),
                   ),
                   Container(
-                    width: 75.0,
-                    height: 75.0,
+                    width: 60.0,
+                    height: 60.0,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(25.0),
                         image: DecorationImage(
                             image: AssetImage('assets/x.jpeg'))),
                   ),
                   Container(
-                    width: 75.0,
-                    height: 75.0,
+                    width: 60.0,
+                    height: 60.0,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(25.0),
                         image: DecorationImage(
@@ -334,8 +333,8 @@ class FriendComponent extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(left: 10.0),
                     child: Container(
-                      width: 75.0,
-                      height: 75.0,
+                      width: 60.0,
+                      height: 60.0,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20.0),
                         color: Colors.black38,
